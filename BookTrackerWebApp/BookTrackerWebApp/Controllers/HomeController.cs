2f266a18c1f0ac84bb4ad4fb8b47493d0ef3710a
Katherine Hambley <superdesigngirl@mac.com>
2020-01-21 14:42:50 -0600

    Added HomeController and View

---
+++ BookTrackerWebApp/BookTrackerWebApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BookTrackerWebApp.Models;
+
+namespace BookTrackerWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+        public HomeController(IRepository repo) => repository = repo;
+
+        public IActionResult Index() => View(repository.Books);
+
+        [HttpPost]
+        public IActionResult AddBook(Book book)
+        {
+            repository.AddBook(book);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
