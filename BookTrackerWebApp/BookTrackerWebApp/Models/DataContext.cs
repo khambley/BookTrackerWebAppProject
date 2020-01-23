db890d89f1e73fb8c9fb6b057c202eebc4994953
khambley <superdesigngirl@mac.com>
2020-01-22 20:37:14 -0600

    Added EF pkg and appsettings.json file

---
+++ BookTrackerWebApp/BookTrackerWebApp/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookTrackerWebApp.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opts) : base(opts) { }
+
+        public DbSet<Book> Books { get; set; }
+
+        }
+    }
+}
